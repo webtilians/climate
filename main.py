02acbcf88f4c127f5318b1220efb35acf85440d6
webtilians <enrikelr@hotmail.com>
2024-12-29 17:35:23 +0100

    configuracion inicial proyecto

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    print("Bienvenido al proyecto de Climatización Inteligente.")
+    print("Configuración inicial completa. Listo para generar datos.")
+
+if __name__ == "__main__":
+    main()
